332c0e738dbd35809f8805df7053fef95e711b5e
Rahul Krishna <rkbalu524@gmail.com>
2024-03-27 12:23:34 +0530

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/thisdot/Notion-DoIt-4db30df6593b4c339f9d067759916964?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
